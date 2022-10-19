eb1ae9f00b41ce808237835e5f98cb8acd28f7ba
alxevvv <alxevvv@gmail.com>
2022-10-19 16:13:15 +0600

    setup pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
