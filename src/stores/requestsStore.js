b31360f8ed5392f065fc46bfd271801a5be1275c
alxevvv <alxevvv@gmail.com>
2022-10-22 09:12:06 +0600

    cardano db querying helpers

---
+++ src/stores/requestsStore.js
@@ -0,0 +1,85 @@
+import { reactive, readonly } from "vue";
+import { defineStore } from "pinia";
+
+export const useRequestsStore = defineStore(
+  "requests",
+  () => {
+    const requests = reactive({});
+
+    function onBeforeRequest(url) {
+      requests[url] = {
+        requestedAt: Date.now(),
+        isLoading: true,
+      };
+    }
+
+    function onAfterResponse(url, error, data) {
+      Object.assign(requests[url], {
+        respondedAt: Date.now(),
+        isLoading: false,
+        isSuccess: !error,
+        isFailed: !!error,
+        error,
+        data,
+      });
+    }
+
+    function createError(response, data) {
+      return {
+        statusCode: null,
+        statusText: "",
+        data: {
+          code: "",
+          details: null,
+          hint: "",
+          message: "",
+          ...(data || {}),
+        },
+        ...(response || {}),
+      };
+    }
+
+    async function sendRequest(url, processResponse = async (response) => await response.json()) {
+      let response;
+      let error = null;
+      let data = null;
+
+      onBeforeRequest(url);
+
+      try {
+        response = await fetch(url);
+      } catch (err) {
+        error = createError(null, { message: `Data fetching error: ${err.toString()}` });
+      }
+
+      if (!error) {
+        if (!response.ok) {
+          const errorResponse = {
+            statusCode: response.status,
+            statusText: response.statusText,
+          };
+          try {
+            const errorDetails = await processResponse(response);
+            error = createError(errorResponse, errorDetails);
+          } catch {
+            error = createError(errorResponse, { message: "Could not get error details" });
+          }
+        } else {
+          try {
+            data = await processResponse(response);
+          } catch (err) {
+            error = createError(null, { message: `Response processing error: ${err.toString()}` });
+          }
+        }
+      }
+
+      onAfterResponse(url, error, data);
+    }
+
+    return {
+      requests: readonly(requests),
+      sendRequest,
+    };
+  },
+  { persist: false },
+);
