670e50558f15998de31eb719d0c21a5d17c903da
alxevvv <alxevvv@gmail.com>
2022-10-23 13:30:56 +0600

    copy/save genesis JSON

---
+++ src/utils/saveToFile.js
@@ -0,0 +1,7 @@
+export default function saveToFile(content, fileName = "download.txt", contentType = "text/plain") {
+  const a = document.createElement("a");
+  const file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
--- src/views/PageFundGenesis.vue
+++ src/views/PageFundGenesis.vue
@@ -33,7 +33,30 @@
       class="progress is-small is-primary"
       max="100"
     />
-    <pre v-else>{{ fundGenesis }}</pre>
+    <div v-else>
+      <pre class="block">{{ fundGenesis }}</pre>
+
+      <div class="block buttons">
+        <button
+          class="button is-info"
+          @click="copyGenesisToClipboard"
+        >
+          <span class="icon is-small">
+            <i class="fas fa-clipboard" />
+          </span>
+          <span>Copy to clipboard</span>
+        </button>
+        <button
+          class="button is-info"
+          @click="saveGenesisToFile"
+        >
+          <span class="icon is-small">
+            <i class="fas fa-file-download" />
+          </span>
+          <span>Save to file</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,12 +64,46 @@
 import { ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import { useFundsStore } from "@/stores/fundsStore";
+import { useNotificationsStore } from "@/stores/notificationsStore";
+import saveToFile from "@/utils/saveToFile";
 
 const route = useRoute();
 const fundsStore = useFundsStore();
-const fundHash = route.params.hash;
+const notificationsStore = useNotificationsStore();
 
+const fundHash = route.params.hash;
 const fundGenesis = ref(null);
+
+async function copyGenesisToClipboard() {
+  if (!fundGenesis.value) {
+    return;
+  }
+  try {
+    await navigator.clipboard.writeText(JSON.stringify(fundGenesis.value, null, 2));
+    notificationsStore.add({
+      type: "is-success",
+      text: "Fund Genesis JSON copied to clipboard",
+      duration: 2000,
+    });
+  } catch (err) {
+    notificationsStore.add({
+      type: "is-danger",
+      text: `Error copying to clipboard: ${err.toString()}`,
+      duration: 5000,
+    });
+  }
+}
+
+function saveGenesisToFile() {
+  if (!fundGenesis.value) {
+    return;
+  }
+  saveToFile(
+    JSON.stringify(fundGenesis.value, null, 2),
+    `${fundGenesis.value.title}.json`,
+    "application/json",
+  );
+}
 
 watch(
   () => fundsStore.all,
