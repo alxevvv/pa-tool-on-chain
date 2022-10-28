8b482a3091c17f5702a371725ba2dbba8a44d449
alxevvv <alxevvv@gmail.com>
2022-10-28 21:51:50 +0600

    extend dayjs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import duration from "dayjs/plugin/duration";
+import utc from "dayjs/plugin/utc";
+
 import { createApp } from "vue";
+
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
+
 import App from "./App.vue";
 import router from "./router";
+
+dayjs.extend(relativeTime);
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
+dayjs.extend(utc);
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
